747ba345ab26212ea0a02027999c74039a3eb5c5
Ayrtoon_Tintaya <ayrtoon.tintaya@tecsup.edu.pe>
2024-04-06 20:39:03 -0500

    Agregando Docker

--- main.js
+++ main.js
@@ -100,7 +100,7 @@ app.delete("/productos/:id", (req, res) => {
     res.json({ mensaje: "Producto eliminado exitosamente" });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3030;
 app.listen(port, () => {
     console.log("Servidor Express corriendo en el puerto", port);
 });
